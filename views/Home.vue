8c999e579f7abb509b74884deeeb0ef608c98561
jeson_lam <jeson_lam@163.com>
2022-08-14 21:03:34 +0800

    cssstyle

--- views/Home.vue
+++ views/Home.vue
@@ -1,11 +1,25 @@
 <template>
-    <div>我是Home页面</div>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
-    name: 'Home',
-    data() {
-        return {}
-    }
-}
+  name: "Home",
+  data() {
+    return {};
+  },
+};
 </script>
+<style>
+.el-header {
+    background-color: #333;
+}
+.el-main {
+    padding-top: 0;
+}
+</style>
