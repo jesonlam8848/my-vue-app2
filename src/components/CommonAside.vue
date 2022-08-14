9d654fb5bccfe8f47d801e923718566f98b26233
jeson_lam <jeson_lam@163.com>
2022-08-14 21:46:18 +0800

    一级menu

---
+++ src/components/CommonAside.vue
@@ -0,0 +1,104 @@
+<template>
+  <el-menu
+    default-active="1-4-1"
+    class="el-menu-vertical-demo"
+    @open="handleOpen"
+    @close="handleClose"
+    :collapse="isCollapse"
+  >
+    <el-menu-item
+      v-for="item in noChildren"
+      :index="item.path"
+      :key="item.path"
+    >
+      <i class="'el-icon-' + item.icon"></i>
+      <span slot="title">{{ item.label }}</span>
+    </el-menu-item>
+    <el-submenu index="1">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span slot="title">导航一</span>
+      </template>
+      <el-menu-item-group>
+        <span slot="title">分组一</span>
+        <el-menu-item index="1-1">选项1</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      isCollapse: false,
+      menu: [
+        {
+          path: "/",
+          name: "home",
+          label: "首页",
+          icon: "s-home",
+          url: "Home/Home",
+        },
+        {
+          path: "/mall",
+          name: "mall",
+          label: "商品管理",
+          icon: "video-play",
+          url: "MallManage/MallManage",
+        },
+        {
+          path: "/user",
+          name: "user",
+          label: "用户管理",
+          icon: "user",
+          url: "UserManage/UserManage",
+        },
+        {
+          label: "其他",
+          icon: "location",
+          children: [
+            {
+              path: "/page1",
+              name: "page1",
+              label: "页面1",
+              icon: "setting",
+              url: "Other/PageOne",
+            },
+            {
+              path: "/page2",
+              name: "page2",
+              label: "页面2",
+              icon: "setting",
+              url: "Other/PageTwo",
+            },
+          ],
+        },
+      ],
+    };
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+  computed: {
+    noChildren() {
+      return this.menu.filter((item) => !item.children);
+    },
+    hasChildren() {
+      return this.menu.filter((item) => item.children);
+    },
+  },
+};
+</script>
--- views/Home.vue
+++ views/Home.vue
@@ -1,6 +1,8 @@
 <template>
-  <el-container>
-    <el-aside width="200px">Aside</el-aside>
+  <el-container style="height: 100%">
+    <el-aside width="auto">
+      <common-aside></common-aside>
+    </el-aside>
     <el-container>
       <el-header>Header</el-header>
       <el-main>Main</el-main>
@@ -8,8 +10,12 @@
   </el-container>
 </template>
 <script>
+import CommonAside from '../src/components/CommonAside.vue';
 export default {
   name: "Home",
+  components: {
+    CommonAside
+  },
   data() {
     return {};
   },
